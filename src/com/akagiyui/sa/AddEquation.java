4807125a8322f622c85e234a8188b0d1a7603de6
AkagiYui <ddio001@163.com>
2022-11-07 15:58:34 +0800

    fix: code format

--- src/com/akagiyui/sa/AddEquation.java
+++ src/com/akagiyui/sa/AddEquation.java
@@ -4,7 +4,6 @@ package com.akagiyui.sa;
  * 加法等式类
  */
 public class AddEquation extends Equation {
-
     public AddEquation(short operand1, short operand2) {
         super(operand1, operand2, '+');
     }
--- src/com/akagiyui/sa/SubEquation.java
+++ src/com/akagiyui/sa/SubEquation.java
@@ -4,7 +4,6 @@ package com.akagiyui.sa;
  * 减法等式类
  */
 public class SubEquation extends Equation {
-
     public SubEquation(short operand1, short operand2) {
         super(operand1, operand2, '-');
     }
