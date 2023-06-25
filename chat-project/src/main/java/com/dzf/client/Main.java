ba6483d5f898decdc8cc67e3d5eb18211cd772ed
AkagiYui <akagiyui@yeah.net>
2023-06-26 01:08:02 +0800

    feat: add chat project file

---
+++ chat-project/src/main/java/com/dzf/client/Main.java
@@ -0,0 +1,11 @@
+package com.dzf.client;
+
+/**
+ * @author AkagiYui
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ chat-project/src/main/java/com/dzf/server/Main.java
@@ -0,0 +1,12 @@
+package com.dzf.server;
+
+/**
+ * 聊天室 服务端
+ * @author AkagiYui
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
